2630fc2a7a6b91bb17aa95198e0d6a5fac0cb4e2
vladimirsan <vladimirsan@gmail.com>
2019-10-29 14:04:18 -0700

    day 02

---
+++ curriculum/day-02/app.js
@@ -0,0 +1,65 @@
+'use strict';
+
+var doesUserLikePizza = confirm('Do you like Pizza?');
+
+// If the user likes pizza. he/she has to buy me some. If not, they have to go away from my sight
+
+// if (doesUserLikePizza === true) {
+//   console.log('Great. Buy me some artesanal pizza');
+// } else {
+//   console.log('oh.. :(');
+// }
+
+var name = prompt('What is your name');
+
+if (name === 'kali') {
+
+  console.log('You are the best doggo!');
+
+} else if(name === 'ginger'){
+
+  console.log('You are the second best doggo! (almost as cute as kali though)');
+
+} else if(doesUserLikePizza === true) {
+
+  console.log('Great. Buy me some artesanal pizza');
+
+ } else {
+
+  console.log('oh.. :(');
+
+ }
+
+// Vinicio - this will be a separate chain from next if
+// if (name === 'kali') {
+//   console.log('You are the best doggo!');
+// } 
+
+// if(name === 'ginger'){
+//   console.log('You are the second best doggo! (almost as cute as kali though)');
+// }
+
+// if(doesUserLikePizza === true) {
+//   console.log('Great. Buy me some artesanal pizza');
+//  } else {
+//   console.log('oh.. :(');
+//  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+var names = ['Jon', 'Dany', 'Robert', 'Jorah'];
+
+
+
+
+
