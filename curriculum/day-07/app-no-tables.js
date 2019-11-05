c3bc8d5c9f9879dd128ca66ee649b0befd81b8ad
vladimirsan <vladimirsan@gmail.com>
2019-11-05 12:00:54 -0800

    day 07

---
+++ curriculum/day-07/app-no-tables.js
@@ -0,0 +1,79 @@
+'use strict'; 
+
+function Animal(name, barksPerWeek, weight) {
+  // VInicio - everytime I do this. I'm adding a copy of something into 'newborn' object
+  this.name = name;
+  this.barksPerWeek = barksPerWeek;
+  this.weight = weight;
+
+  this.renderBarks = function(domReference) {
+    for(var barkIndex = 0; barkIndex < this.barksPerWeek.length; barkIndex++) {
+      var barkParagraph = document.createElement('p');
+
+      barkParagraph.textContent = this.barksPerWeek[barkIndex];
+      domReference.append(barkParagraph);
+    }
+  }
+
+  this.renderName = function(domReference) {
+    var paragrah = document.createElement('p');
+
+    paragrah.textContent = this.name;
+    domReference.append(paragrah);
+  }
+
+  this.render = function(domReference) {
+    // Vinicio - what is domReference? - A an anchor to the dom, that I can use to render things
+
+    this.renderName(domReference);
+    this.renderBarks(domReference);
+  }
+  // VInicio - javascript will do this for you if you call your function with new
+  // return this;
+}
+
+var kali = new Animal('Kali', [10,12,23], '60 lbs');
+var khal = new Animal('Khal', [0,100,0], '7 lbs');
+var ginger = new Animal('Ginger', [1,1,2], '25 lbs');
+ 
+var domReference = document.getElementById('dog-data');
+var animals = [kali, khal, ginger];
+
+for(var animalIndex = 0; animalIndex < animals.length; animalIndex++) {
+  var currentAnimal = animals[animalIndex];
+
+  currentAnimal.render(domReference);
+  // store.render();
+} 
+
+
+function renderDaysOfTheWeek(domReference) {
+  // <tr> - :)
+  //   <th> Header 1</th> :)
+  //   <th> Header 2</th> :)
+  //   <th> Header 3</th> :)
+  // </tr> :)
+  var daysOfTheWeek = ['','Monday', 'Tuesday', 'Wednesday'];
+
+  var tr = document.createElement('tr');
+
+  for(var day = 0; day < daysOfTheWeek.length; day++) {
+    var th = document.createElement('th');
+
+    th.textContent = daysOfTheWeek[day];
+
+    tr.append(th);
+  }
+  domReference.append(tr);
+}
+
+// Connect to the table
+
+var tableReference = document.getElementById('dog-table');
+
+renderDaysOfTheWeek(tableReference);
+
+
+// Render the days of the week
+// Render the dogs name and its barks in the same rowe
+// go to the next dog
