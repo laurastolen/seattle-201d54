5171b9f55592e229a302a3f1e50391b7923341ef
vladimirsan <vladimirsan@gmail.com>
2019-11-08 11:57:25 -0800

    day 10

---
+++ curriculum/day-10/app.js
@@ -0,0 +1,117 @@
+'use strict'; 
+
+
+// function bug() {
+
+//   function render() {
+//     return 'rendering;'
+//   }
+//   return render();
+
+//   function render() {
+//     return 'oh boy :/';
+//   }
+// }
+// function bug() {
+//   function render() {
+//     return 'rendering;'
+//   }
+//   return render();
+//   function render() {
+//     return 'oh boy :/';
+//   }
+// }
+
+// Vinicio - avoid having big functions
+// Vinicio - avoid having functions inside functions
+// Vinicio - pick a style and be as consistent as you can
+// function bug() {
+//   var render = function () {
+//     return 'rendering;'
+//   }
+//   return render();
+//   render = function() {
+//     return 'oh boy :/';
+//   }
+
+// }
+//  console.log(bug());
+
+// -----------------------------------------------------
+// -----------------------------------------------------
+// Function Expressions
+// function functionWeird() {
+//   var add = function(a, b) {
+//     return a + b;
+//   };
+//   // Vinicio - BEFORE using a name, make sure the name
+//   // has been PROPERLY defined
+//   var result = add(5,6);
+// }
+
+// console.log(functionWeird());
+// function functionWeird() {
+//   var result = undefined;
+//   var add = undefined;
+
+//   result = add(5,6);
+//   add = function(a, b) {
+//     return a + b;
+//   };
+
+//   return result;
+// }
+
+// Function declaration
+// function functionWeird() {
+//   var result = add(5,6);
+
+//   function add(a,b) {
+//     return a + b;
+//   }
+//   return result;
+// }
+
+// console.log(functionWeird());
+
+// function functionWeird() {
+//   var result = undefined;
+
+//   function add(a,b) {
+//     return a + b;
+//   }
+
+//   result = add(5,6);
+//   return result;
+// }
+
+// -----------------------------------------------------
+// -----------------------------------------------------
+// function function1() {
+//   return kali;
+//   var kali = 'is cute';
+//   // var kali = undefined;
+//   // return kali;
+//   // kali = 'is cute';
+// }
+
+// console.log(function1());
+
+// -----------------------------------------------------
+// -----------------------------------------------------
+// function1();
+
+// function function1() {
+//   console.log('test');
+// }
+// -----------------------------------------------------
+// -----------------------------------------------------
+// Vinicio - when javascript sees this
+// console.log(pizza);
+// var pizza = 'is yummy';
+
+// Vinicio -  it actually executes this
+// var pizza = undefined;
+// console.log(pizza);
+// pizza = 'is yummy';
+// -----------------------------------------------------
